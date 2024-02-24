24288c686d5d369b5109f0dc061f471b8a129f60
Pawel Kawa <pawel.kawa@classroomsecrets.co.uk>
2024-02-24 22:44:30 +0000

    user can add task to todo list

--- resources/js/Pages/Calendar.vue
+++ resources/js/Pages/Calendar.vue
@@ -1,13 +1,40 @@
 <template>
-    <div>
+    <div class="p-4">
         <!-- <Calendar /> -->
-        <DatePicker v-model="date" mode="date" is-required :attributes="attributes" />
+        <div class="flex justify-center">
+            <DatePicker v-model="date" mode="date" is-required :attributes="attributes" class="mx-auto" />
+        </div>
+
+        <div class="bg-white mt-8 p-4">
+            <p class="text-center text-3xl">{{ formattedDate }}</p>
+            <p>Things to do:</p>
+            <div>
+                <ul>
+                    <li v-for="(todo, index) in todos" :key="index">
+                        {{ index + 1 }}. {{ todo }}
+                    </li>
+                </ul>
+            </div>
+            <div class="flex justify-center gap-5">
+                <input class="border w-full px-2 py-1" type="text" v-model="singleTask" @keydown.enter="addTask(singleTask)">
+                <button @click="addTask(singleTask)" 
+                    class=" w-40 bg-green-200 py-2 text-center rounded">
+                    Add task <i class="fa-solid fa-plus ml-4"></i>
+                </button>
+            </div>
+            <div class="mt-8 flex justify-center">
+                <button @click="saveTasks" 
+                    class="bg-green-700 py-1 px-4 text-white rounded">
+                    Save tasks
+                </button>
+            </div>
+        </div>
     </div>
 </template>
     
 <script>
 import { Calendar, DatePicker } from 'v-calendar';
-// import 'v-calendar/style.css';
+import 'v-calendar/style.css';
 import { debounce } from 'lodash';
 
 export default {
@@ -15,13 +42,11 @@ export default {
         Calendar,
         DatePicker,
     },
-    mounted() {
-        // Debounce the handleDateChange method with a delay of 500ms
-        this.debouncedHandleDateChange = debounce(this.handleDateChange, 500);
-    },
     data() {
         return {
             date: new Date(),
+            singleTask: "",
+            todos: [],
             attributes: [
                 {
                     dot: true,
@@ -43,7 +68,28 @@ export default {
             ]
         };
     },
+    mounted() {
+        // Debounce the handleDateChange method with a delay of 500ms
+        this.debouncedHandleDateChange = debounce(this.handleDateChange, 500);
+    },
+    computed: {
+        formattedDate() {
+            return this.date.toLocaleDateString('en-US', {
+                weekday: 'long',
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit'
+            });
+        }
+    },
     methods: {
+        addTask(task){
+            this.todos.push(task);
+            this.singleTask = "";
+        },
+        saveTasks() {
+            console.log(this.todos);
+        },
         handleDateChange(date) {
             console.log('Selected date:', date);
             // Call your backend API here
